d7a6f18d57a121d4cbea80bdf3bca97394214a4f
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2022-02-18 17:07:50 +0530

    Closing #2

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,89 @@
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "Route is working fine" })
+const Product = require("../models/productModel")
+
+
+
+
+//Create product--ADMIN
+
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body)
+
+  res.status(201).json({
+    success: true,
+    product
+  })
+}
+
+
+//Get all product
+exports.getAllProducts = async (req, res) => {
+
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products
+  })
+}
+
+//update product
+exports.updateProduct = async (req, res, next) => {
+
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    })
+  } else {
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false
+    })
+  }
+  res.status(200).json({
+    success: true,
+    product
+  })
+}
+
+//delete product
+
+exports.deleteProduct = async (req, res, next) => {
+
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    })
+  } else {
+    await product.remove()
+  }
+
+  res.status(200).json({
+    success: true,
+    message: "Product deleted successfully"
+  })
+}
+
+//get single product
+exports.getProductDetails = async (req, res, next) => {
+
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(500).json({
+      success: false,
+      message: "Product not found"
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product
+  })
 }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,8 +1,17 @@
 const express = require("express");
-const { getAllProducts } = require("../controllers/productController");
+const { getAllProducts, createProduct, updateProduct, deleteProduct, getProductDetails } = require("../controllers/productController");
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts)
+
+
+router.route("/products/new").post(createProduct)
+
+router.route("/products/:id").put(updateProduct)
+
+router.route("/products/:id").delete(deleteProduct)
+
+router.route("/products/:id").get(getProductDetails)
 
 module.exports = router
