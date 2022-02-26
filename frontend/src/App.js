3474d784fc584f84b26068963a154089e07b4e88
sidiiitg123 <sudhanshuupadhyay457@gmail.com>
2022-02-26 17:23:55 +0530

    Closing #19

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Home from "./component/Home/Home"
 import ProductDetails from './component/Product/ProductDetails';
 import Products from './component/Product/Products';
 import Search from "./component/Product/Search"
+import LoginSignUp from './component/User/LoginSignUp';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
       <Route exact path="/products" component={Products} />
       <Route exact path="/search" component={Search} />
       <Route exact path="/products/:keyword" component={Products} />
+      <Route exact path="/login" component={LoginSignUp} />
 
       <Footer />
     </Router>
